413e3be2a432b4513b3cc84699b076af0b9477f2
Pascal-Juma <pascaljuma2002@gmail.com>
2025-05-13 22:33:21 +0300

    Update the about section

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,31 +2,64 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import HorizontalRuleIcon from "@mui/icons-material/HorizontalRule";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import ReactPlayer from "react-player";
+import { FaBriefcase } from "react-icons/fa";
+import { SlCalender } from "react-icons/sl";
 import game from "../../assets/game.mp4";
+import "./About.css";
 
 function About() {
+  const [text] = useTypewriter({
+    words: ["About Me"],
+    loop: true,
+    typeSpeed: 150,
+    deleteSpeed: 150,
+  });
   return (
     <>
       <Stack
-        mt={7}
         mb={7}
-        gap={5}
         direction={{ xs: "column", sm: "column", md: "column", lg: "row" }}
         sx={{
           marginInline: {
-            xs: 3,
-            sm: 4,
+            xs: 5,
+            sm: 5,
             md: 5,
             lg: 11,
           },
+          display: "flex",
+          justifyContent: {
+            xs: "center",
+            sm: "center",
+            md: "center",
+            lg: "center",
+          },
+          alignItems: {
+            xs: "center",
+            sm: "center",
+            md: "center",
+            lg: "flex-start",
+          },
+          gap: {
+            xs: 6,
+            sm: 6,
+            md: 8,
+            lg: 5,
+          },
+          mt: {
+            xs: 13,
+            sm: 10,
+            md: 10,
+            lg: 7,
+          },
         }}
       >
-        <Box pt={6.5}>
+        <Box pt={3} width="73%">
           <ReactPlayer
             url={game}
-            height="auto"
-            width="450px"
+            height="100%"
+            width="100%"
             playing={true}
             loop={true}
             volume={0}
@@ -37,15 +70,85 @@ function About() {
               boxShadow: "0px 0px 10px rgba(148, 143, 143, 0.9)",
             }}
           />
+          <Box
+            mt={2}
+            gap={7}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              color="var(--green-color)"
+              fontSize="55px"
+              display="flex"
+              gap={1}
+              width="100%"
+            >
+              <FaBriefcase style={{ marginTop: "5px" }} />
+              <Box >
+                <Box>
+                  <Typography
+                    fontSize="30px"
+                    color="var(--light-grey)"
+                    fontFamily="var(--prime-font)"
+                    fontWeight={600}
+                  >
+                    20+
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography
+                    fontSize="13px"
+                    color="var(--light-grey)"
+                    fontFamily="var(--prime-font)"
+                  >
+                    Completed Projects
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+            <Box
+              color="var(--green-color)"
+              fontSize="55px"
+              display="flex"
+              gap={1}
+              width="90%"
+            >
+              <SlCalender style={{ marginTop: "5px" }} />
+              <Box >
+                <Box>
+                  <Typography
+                    fontSize="30px"
+                    color="var(--light-grey)"
+                    fontFamily="var(--prime-font)"
+                    fontWeight={600}
+                  >
+                    2+
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography
+                    fontSize="13px"
+                    color="var(--light-grey)"
+                    fontFamily="var(--prime-font)"
+                  >
+                    Yrs of Experience
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
         </Box>
-        <Box color="var(--light-grey)">
+        <Box color="var(--light-grey)" width="100%">
           <Typography
             fontFamily="var(--prime-font)"
             color="var(--green-color)"
             fontSize="31px"
           >
             <HorizontalRuleIcon />
-            About Me
+            {text}
+            <Cursor />
           </Typography>
           <Typography
             variant="body1"
